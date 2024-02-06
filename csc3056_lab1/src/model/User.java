34c83b6c06c9e622796983111d4baaa2f4de32a5
40303020 <40303020@ads.qub.ac.uk>
2024-02-06 13:52:13 +0000

    TODO 2 injecting intentional defect into User

--- csc3056_lab1/src/model/User.java
+++ csc3056_lab1/src/model/User.java
@@ -10,7 +10,7 @@ public class User {
 	public User(String username, String password, String first_name, String last_name, String mobile_number) {
 		super();
 		this.username = username;
-		this.password = password;
+		//this.password = password; injected intentional defect : commented line out 
 		this.first_name = first_name;
 		this.last_name = last_name;
 		this.mobile_number = mobile_number;
