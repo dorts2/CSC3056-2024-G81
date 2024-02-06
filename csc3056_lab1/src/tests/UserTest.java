bc8528f85fdd9e39868e64089e846ba72a723047
odhranfriel <ofriel02@qub.ac.uk>
2024-02-06 14:19:24 +0000

    TODO4: using assertions to test User methods

--- csc3056_lab1/src/tests/UserTest.java
+++ csc3056_lab1/src/tests/UserTest.java
@@ -15,31 +15,45 @@ public class UserTest {
 				
 		User testUser = new User(test_username, test_password, test_first_name, test_last_name, test_mobile_number);
 		
-		if (testUser.getUsername() == test_username)
-			System.out.println(TestUtils.TEXT_COLOR_GREEN + "TestConstructor-TC1 passed" + TestUtils.TEXT_COLOR_RESET);
-		else
-			System.out.println(TestUtils.TEXT_COLOR_RED + "TestConstructor-TC1 failed: username did not match" + TestUtils.TEXT_COLOR_RESET);
+//		if (testUser.getUsername() == test_username)
+//			System.out.println(TestUtils.TEXT_COLOR_GREEN + "TestConstructor-TC1 passed" + TestUtils.TEXT_COLOR_RESET);
+//		else
+//			System.out.println(TestUtils.TEXT_COLOR_RED + "TestConstructor-TC1 failed: username did not match" + TestUtils.TEXT_COLOR_RESET);
+//		
+//		if (testUser.getPassword() == test_password)
+//			System.out.println(TestUtils.TEXT_COLOR_GREEN + "TestConstructor-TC2 passed" + TestUtils.TEXT_COLOR_RESET);
+//		else
+//			System.out.println(TestUtils.TEXT_COLOR_RED + "TestConstructor-TC2 failed: password did not match" + TestUtils.TEXT_COLOR_RESET);
+//		
+//		if (testUser.getFirst_name() == test_first_name)
+//			System.out.println(TestUtils.TEXT_COLOR_GREEN + "TestConstructor-TC3 passed" + TestUtils.TEXT_COLOR_RESET);
+//		else
+//			System.out.println(TestUtils.TEXT_COLOR_RED + "TestConstructor-TC3 failed: first name did not match" + TestUtils.TEXT_COLOR_RESET);
+//		
+//		if (testUser.getLast_name() == test_last_name)
+//			System.out.println(TestUtils.TEXT_COLOR_GREEN + "TestConstructor-TC4 passed" + TestUtils.TEXT_COLOR_RESET);
+//		else
+//			System.out.println(TestUtils.TEXT_COLOR_RED + "TestConstructor-TC4 failed: last name did not match" + TestUtils.TEXT_COLOR_RESET);
+//		
+//		if (testUser.getMobile_number() == test_mobile_number)
+//			System.out.println(TestUtils.TEXT_COLOR_GREEN + "TestConstructor-TC5 passed" + TestUtils.TEXT_COLOR_RESET);
+//		else
+//			System.out.println(TestUtils.TEXT_COLOR_RED + "TestConstructor-TC5 failed: mobile number did not match" + TestUtils.TEXT_COLOR_RESET);
 		
-		if (testUser.getPassword() == test_password)
-			System.out.println(TestUtils.TEXT_COLOR_GREEN + "TestConstructor-TC2 passed" + TestUtils.TEXT_COLOR_RESET);
-		else
-			System.out.println(TestUtils.TEXT_COLOR_RED + "TestConstructor-TC2 failed: password did not match" + TestUtils.TEXT_COLOR_RESET);
 		
-		if (testUser.getFirst_name() == test_first_name)
-			System.out.println(TestUtils.TEXT_COLOR_GREEN + "TestConstructor-TC3 passed" + TestUtils.TEXT_COLOR_RESET);
-		else
-			System.out.println(TestUtils.TEXT_COLOR_RED + "TestConstructor-TC3 failed: first name did not match" + TestUtils.TEXT_COLOR_RESET);
 		
-		if (testUser.getLast_name() == test_last_name)
-			System.out.println(TestUtils.TEXT_COLOR_GREEN + "TestConstructor-TC4 passed" + TestUtils.TEXT_COLOR_RESET);
-		else
-			System.out.println(TestUtils.TEXT_COLOR_RED + "TestConstructor-TC4 failed: last name did not match" + TestUtils.TEXT_COLOR_RESET);
+		assert testUser.getUsername() == test_username;
+		assert testUser.getPassword() == test_password;
+		assert testUser.getFirst_name() == test_first_name;
+		assert testUser.getLast_name() == test_last_name;
+		assert testUser.getMobile_number() == test_mobile_number;
+//		assert 1==2;
 		
-		if (testUser.getMobile_number() == test_mobile_number)
-			System.out.println(TestUtils.TEXT_COLOR_GREEN + "TestConstructor-TC5 passed" + TestUtils.TEXT_COLOR_RESET);
-		else
-			System.out.println(TestUtils.TEXT_COLOR_RED + "TestConstructor-TC5 failed: mobile number did not match" + TestUtils.TEXT_COLOR_RESET);
+		System.out.println("All Java assertions in the test suite passed (none failed).");
+		
 	}
+	
+		
 	
 		
 	
